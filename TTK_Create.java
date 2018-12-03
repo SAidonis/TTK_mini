b58b31e92ab24bf60079d6043cc1bb66b404ea30
asifmt786 <asifmt786@yahoo.co.uk>
2018-12-03 11:47:04 +0100

    Added code for create username

--- TTK_Create.java
+++ TTK_Create.java
@@ -1,3 +1,16 @@
+import java.util.Scanner;
+import java.io.File;
+import java.util.Scanner;
 public class TTK_Create {
-	
+public static void main(String[] args) {
+	Scanner input = new Scanner(System.in);
+	System.out.print("Enter the desire User name: ");
+	String userName = input.nextLine();
+	userName = userName + " .txt";
+
+	TTK_User user = new TTK_User(userName, userName);
+
+
+ }
+
 }
