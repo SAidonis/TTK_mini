c7dc7b749ca8073ac730384b1c90f8bea26bfe58
ioana <ioana.valentir@gmail.com>
2018-12-03 09:55:46 +0100

    I have added code in user file, constructor and get set methods. not done yet

--- TTK_User.java
+++ TTK_User.java
@@ -1,3 +1,30 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class TTK_User{
+  private String password;
+  private String username;
+  List<String> userList = new ArrayList<String>();
+
+  public TTK_User(String u, String p){
+    username = u;
+    password = p;
+  }
+
+  public String getUsername(){
+    return this.username;
+  }
+
+  public String getPassword(){
+    return this.username;
+  }
+
+  public void setUsername(TTK_User user, String u){
+    user.name = u;
+  }
+
+  public void setPassword(TTK_User pass, String p){
+    user.pass = p;
+  }
 
 }
