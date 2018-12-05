9b97678e4257d5cd05776c6eec92d9ab5c7d58dc
asifmt786 <asifmt786@yahoo.co.uk>
2018-12-05 12:00:27 +0100

    updated create file

--- TTK_Create.java
+++ TTK_Create.java
@@ -1,16 +1,15 @@
-import java.util.Scanner;
-import java.io.File;
-import java.util.Scanner;
 public class TTK_Create {
-public static void main(String[] args) {
-	Scanner input = new Scanner(System.in);
-	System.out.print("Enter the desire User name: ");
-	String userName = input.nextLine();
-	userName = userName + " .txt";
 
-	TTK_User user = new TTK_User(userName, userName);
+	public static TTK_User Create(String username, String password) {
 
+		for(TTK_User user : User.userList){
+			if(user.getUsername().equals(username)); //Checks if the user exist
+			return null;
+		}
 
- }
+		TTK_User user = new TTK_User(username, password);
+		User.userList.add(user); //Adding nwe user
+		return user;
+	  }
 
 }
