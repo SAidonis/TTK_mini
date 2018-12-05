a829927bdcad1ac45f40d2f16a1f9df738244892
Ramisa <ramisa.tahsin98gmail.com>
2018-12-05 12:08:06 +0100

    Verify code

--- TTK_Login.java
+++ TTK_Login.java
@@ -1,5 +1,18 @@
 public class TTK_Login {
-  TTK_User user = new TTK_User();
+  private TTK_User user = new TTK_User();
+  user.getUsername();
+  user.getPassword();
+private boolean verifyUser(String username, String password){
 
-  public void
+
+
+user.getUsername == username;
+user.getPassword == password;
+
+
+}
+
+//  TTK_Main user = new TTK_User();
+//  TTK_Create user = new TTK_User();
+if (username = username)
 }
