6b889503aa028b5461fd31a341e99943359c5137
Ramisa <ramisa.tahsin98gmail.com>
2018-12-05 12:00:15 +0100

    Verify username and password

--- TTK_Login.java
+++ TTK_Login.java
@@ -1,9 +1,16 @@
-import java.util.Scanner;
-
 public class TTK_Login {
-  TTK_User user = new TTK_User();
+  private TTK_User user = new TTK_User();
   user.getUsername();
   user.getPassword();
+private boolean verifyUser(String username, String password){
+
+
+
+user.getUsername == username;
+user.getPassword == password;
+
+
+}
 
 //  TTK_Main user = new TTK_User();
 //  TTK_Create user = new TTK_User();
