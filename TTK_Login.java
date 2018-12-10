e7554e388862d9e912ac7853688b336662052498
Karin Sörlin <sorlin.karin@gmail.com>
2018-12-10 09:44:05 +0100

    Made changes to login file, and fixed error message in main file

--- TTK_Login.java
+++ TTK_Login.java
@@ -1,28 +1,13 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class TTK_Login {
+  public static TTK_User tryLogin(String username, String password){
 
-  private TTK_User user = new TTK_User();  //anropar TTK_User
-  user.getUsername();
-  user.getPassword();
-x
-  users check = new users(username, password);
-
-  private boolean verifyUser(String username, String password){
-
-  String Username;
-  String Password;
-
-  Password = "username";
-  Username = "password";
-
-//  user.getUsername == username;         strue or false?
-//  user.getPassword == password;
-
-  if(username.equals("username") && (password.equals("username"))){
-  System.out.println("Welcome");
+    for(TTK_User user : TTK_UserStorage.userList){
+      if(user.getUsername().equals(username) && user.getPassword().equals(password));
+      return user;
+    }
+    return null;
   }
-  else {
-  System.out.println("Failed to Login");
-
-  }
-
 }
